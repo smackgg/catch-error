bf9619caa49e812e744574593ae41a374b8c858b
smackgg <smackgg@gmail.com>
2018-01-31 16:48:47 +0800

    add show demo

--- src/dev.js
+++ src/dev.js
@@ -4,8 +4,12 @@ import CatchError from './';
 const catchError = new CatchError();
 catchError.init({
   url: '/api/error/log',
-  showDevtools: true,
+  showDevtools: false,
 });
+
+window.onclick = () => {
+  catchError.show();
+};
 
 const a = 1;
 console.log(a.b.c);
