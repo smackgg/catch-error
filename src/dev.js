60ecc38cf69203f7935577a783edf7ee6a91011f
smackgg <smackgg@gmail.com>
2018-01-31 15:55:18 +0800

    update demo

--- src/dev.js
+++ src/dev.js
@@ -1,13 +1,11 @@
-// import CatchError from '../lib/catch-error.min';
-import CatchError from './index';
-
+// // import CatchError from '../lib/catch-error.min';
+import CatchError from './';
+//
 const catchError = new CatchError();
 catchError.init({
   url: '/api/error/log',
-  // showDevtools: true,
+  showDevtools: true,
 });
 
-setTimeout(() => {
-  const a = 1;
-  console.log(a.b.c);
-}, 100);
+const a = 1;
+console.log(a.b.c);
